b64c5a40b847265fa1ed54c2240da20f779a973d
Chotom <tomaszczochan@gmail.com>
2023-01-04 17:08:20 +0100

    Update spotify_track_features_fetcher.py and add it to pipeline

--- data_processing/fetch/spotify_api/spotify_track_features_fetcher.py
+++ data_processing/fetch/spotify_api/spotify_track_features_fetcher.py
@@ -12,6 +12,10 @@ from shared_utils.utils import PROJECT_DIR
 
 
 class SpotifyTrackFeaturesFetcher(SpotifyFetcher):
+    """
+    Class for fetching audio features for tracks from the Spotify API.
+    """
+
     def __init__(
             self,
             client_id: str,
@@ -19,6 +23,13 @@ class SpotifyTrackFeaturesFetcher(SpotifyFetcher):
             spotify_track_ids_input_filepath: str,
             spotify_track_features_output_filepath: str
     ):
+        """
+        Args:
+            client_id: Spotify API client ID.
+            client_secret: Spotify API client secret.
+            spotify_track_ids_input_filepath: Filepath of the input CSV file containing track IDs to fetch data for.
+            spotify_track_features_output_filepath: Filepath of the output CSV file to store features.
+        """
         super().__init__(client_id, client_secret)
 
         self.input_filepath = spotify_track_ids_input_filepath
@@ -33,29 +44,46 @@ class SpotifyTrackFeaturesFetcher(SpotifyFetcher):
         if os.path.exists(self.output_filepath):
             df_track_ids = pd.read_csv(self.output_filepath)
             self._track_ids = self._track_ids[~self._track_ids.isin(df_track_ids['id'])]
-        self._logger.info(f'Number of albums to fetch track ids: {len(self._track_ids)}')
+        self._logger.info(f'Number of track ids to fetch features: {len(self._track_ids)}')
 
     def fetch(self):
+        """
+        Fetch track features for each track ID in self._track_ids.
+        Send requests to the Spotify API in chunks and store it in output file.
+        """
+
         chunk_size = 100
-        progress_bar = pyprind.ProgBar(int(len(self._track_ids) / chunk_size), stream=sys.stdout)
+        progress_bar = pyprind.ProgBar(int(len(self._track_ids) / chunk_size + 1), stream=sys.stdout)
         for track_ids in self._ids_by_chunks(chunk_size):
             resp: Response = self._send_for_tracks_features(track_ids)
 
             # Handle response
-            if resp.status_code == 429 or resp.status_code == 401:
+            if resp.status_code == 200:
+                self._handle_successful_response(resp, track_ids)
+            elif resp.status_code == 429 or resp.status_code == 401:
                 retry_after = resp.headers.get("Retry-After", "Cannot get value")
                 self._raise_too_many_request_error(retry_after)
-            elif resp.status_code != 200:
-                self._logger.warning(f'Cannot fetch ids {resp.status_code}: {resp.content}.')
             else:
-                self._handle_successful_response(resp, track_ids)
+                self._logger.warning(f'Cannot fetch ids {resp.status_code}: {resp.content}.')
             progress_bar.update()
 
     def _ids_by_chunks(self, chunk_size):
+        """Generate batches of track IDs with given chunk size."""
         for i in range(0, len(self._track_ids), chunk_size):
-            yield self._track_ids[i:i + chunk_size]
+            yield self._track_ids.iloc[i:i + chunk_size]
 
     def _send_for_tracks_features(self, ids: List) -> Response:
+        """
+        Send a request to the Spotify API for track audio features for the provided track ids.
+        Returns a `Response` object containing the response from the API.
+
+        Args:
+            ids: A list of track ids to get audio features for.
+
+        Returns:
+            A `Response` object containing the response from the Spotify API.
+        """
+
         base_url = f'https://api.spotify.com/v1/audio-features?ids={",".join(ids)}'
         headers = {
             'Content-Type': 'application/json',
@@ -64,22 +92,20 @@ class SpotifyTrackFeaturesFetcher(SpotifyFetcher):
         return requests.get(base_url, headers=headers)
 
     def _handle_successful_response(self, resp: Response, track_ids: pd.Series):
+        """
+        Handle a successful response from the Spotify API for track features.
+
+        Args:
+            resp: The response from the Spotify API.
+            track_ids: The track IDs that the response is for.
+        """
+
         batch: List[Dict[str, str]] = []
         tracks_features = TrackFeatureModel(**resp.json()).audio_features
         for i, features in enumerate(tracks_features):
-            if features is not None:
+            if features:
                 batch.append(features.dict())
             else:
                 self._logger.debug(f'Feature not found for {track_ids.values[i]} track.')
         is_file_new = not os.path.exists(self.output_filepath)
         pd.DataFrame(batch).to_csv(self.output_filepath, mode='a', index=False, header=is_file_new)
-
-
-if __name__ == '__main__':
-    search_fetcher = SpotifyTrackFeaturesFetcher(
-        os.getenv('SPOTIFY_CLIENT_ID'),
-        os.getenv('SPOTIFY_CLIENT_SECRET'),
-        f'{PROJECT_DIR}/data/raw/spotify/spotify_tracks_ids.csv',
-        f'{PROJECT_DIR}/data/raw/spotify/spotify_tracks_feature.csv'
-    )
-    search_fetcher.fetch()
--- data_processing/run_pipeline.py
+++ data_processing/run_pipeline.py
@@ -1,44 +1,71 @@
 import os
 
 from data_processing.fetch.spotify_api.spotify_search_album_fetcher import SpotifySearchAlbumFetcher
+from data_processing.fetch.spotify_api.spotify_track_features_fetcher import SpotifyTrackFeaturesFetcher
 from data_processing.fetch.spotify_api.spotify_track_ids_fetcher import SpotifyTrackIDsFetcher
 from shared_utils.utils import PROJECT_DIR
 from data_processing.fetch.rym.rym_data_collection import RymFetcher
 from data_processing.preprocessing.rym_data_processing import RymDataProcessor
 
+# Initialize variables before running pipelines.
 STEPS = {
     'FetchRym': 0,
     'PreprocessRym': 0,
     'SearchSpotifyAlbums': 0,
-    'FetchSpotifyTrackIDs': 1,
+    'FetchSpotifyTrackIDs': 0,
+    'FetchSpotifyTrackFeatures': 1,
 }
+
 START_YEAR = 1980
 END_YEAR = 1980
+
+spotify_client_id = os.getenv('SPOTIFY_CLIENT_ID')
+spotify_client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+
 rym_path = f'{PROJECT_DIR}/data/raw/rym/rym_charts_{START_YEAR}_{END_YEAR}.csv'
 rym_processed_path = f'{PROJECT_DIR}/data/processed/rym/rym_charts_{START_YEAR}_{END_YEAR}.csv'
 spotify_search_path = f'{PROJECT_DIR}/data/raw/spotify/spotify_search_album_id_{START_YEAR}_{END_YEAR}.csv'
 spotify_track_ids_path = f'{PROJECT_DIR}/data/raw/spotify/spotify_tracks_ids_{START_YEAR}_{END_YEAR}.csv'
+spotify_track_features_path = f'{PROJECT_DIR}/data/raw/spotify/spotify_tracks_feature_{START_YEAR}_{END_YEAR}.csv'
 
-if STEPS['FetchRym']:
-    RymFetcher(rym_path).fetch(START_YEAR, END_YEAR)
+# Run pipeline based on declared steps.
+if __name__ == '__main__':
+    if STEPS['FetchRym']:
+        rym_fetcher = RymFetcher(
+            rym_path
+        )
+        rym_fetcher.fetch(START_YEAR, END_YEAR)
 
-if STEPS['PreprocessRym']:
-    RymDataProcessor(rym_path, rym_processed_path).process()
+    if STEPS['PreprocessRym']:
+        rym_data_processor = RymDataProcessor(
+            rym_path,
+            rym_processed_path
+        )
+        rym_data_processor.process()
 
-if STEPS['SearchSpotifyAlbums']:
-    search_fetcher = SpotifySearchAlbumFetcher(
-        os.getenv('SPOTIFY_CLIENT_ID'),
-        os.getenv('SPOTIFY_CLIENT_SECRET'),
-        rym_processed_path,
-        spotify_search_path
-    )
-    search_fetcher.fetch()
+    if STEPS['SearchSpotifyAlbums']:
+        search_fetcher = SpotifySearchAlbumFetcher(
+            spotify_client_id,
+            spotify_client_secret,
+            rym_processed_path,
+            spotify_search_path
+        )
+        search_fetcher.fetch()
 
-if STEPS['FetchSpotifyTrackIDs']:
-    search_fetcher = SpotifyTrackIDsFetcher(
-        os.getenv('SPOTIFY_CLIENT_ID'),
-        os.getenv('SPOTIFY_CLIENT_SECRET'),
-        spotify_search_path,
-        spotify_track_ids_path
-    )
-    search_fetcher.fetch()
+    if STEPS['FetchSpotifyTrackIDs']:
+        search_fetcher = SpotifyTrackIDsFetcher(
+            spotify_client_id,
+            spotify_client_secret,
+            spotify_search_path,
+            spotify_track_ids_path
+        )
+        search_fetcher.fetch()
+
+    if STEPS['FetchSpotifyTrackFeatures']:
+        search_fetcher = SpotifyTrackFeaturesFetcher(
+            spotify_client_id,
+            spotify_client_secret,
+            spotify_track_ids_path,
+            spotify_track_features_path
+        )
+        search_fetcher.fetch()
