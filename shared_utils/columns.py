9f708d233e35dad5c80ea321759ee8cfd9345d61
Chotom <tomaszczochan@gmail.com>
2023-08-04 18:23:24 +0200

    Update columns.py

--- shared_utils/columns.py
+++ shared_utils/columns.py
@@ -3,11 +3,17 @@ ARTIST = 'artist'
 ALBUM = 'album'
 DATE = 'date'
 RATING = 'rating'
-RATING_NUMBER = 'rating_number'
+RATING_NUMBER = 'ratings_number'
 GENRES = 'genres'
 
 RYM_COLS = [ARTIST, ALBUM, DATE, RATING, RATING_NUMBER, GENRES]
 """Columns for rate your music data."""
+
+# RYM FEATURE SELECTION DATA --------------------------------------------------
+RATING_CLASS = 'rating_class'
+DECADE_CLASS = 'decade'
+RYM_FEATURES_COLS = [ARTIST, ALBUM, DECADE_CLASS, RATING]
+"""Column names for features in output RYM file."""
 
 # SPOTIFY RAW DATA ------------------------------------------------------------
 ALBUM_ID = 'album_id'
@@ -18,7 +24,7 @@ PREC_MATCH = 'precision_match'
 SPOTIFY_SEARCH_COLS = [ALBUM, ARTIST, ALBUM_ID, SPOTIFY_ALBUM, SPOTIFY_ARTIST, PREC_MATCH]
 """Column names in output spotify search file."""
 
-# SPOTIFY FEATURES ------------------------------------------------------------
+# SPOTIFY RAW FEATURES --------------------------------------------------------
 DANCEABILITY = 'danceability'
 ENERGY = 'energy'
 KEY = 'key'
@@ -33,16 +39,16 @@ TEMPO = 'tempo'
 DURATION_MS = 'duration_ms'
 TIME_SIGNATURE = 'time_signature'
 
-SPOTIFY_FEATURES = [
+SPOTIFY_RAW_FEATURES = [
     DANCEABILITY, ENERGY, KEY, LOUDNESS, MODE, SPEECHINESS, ACOUSTICNESS,
     INSTRUMENTALNESS, LIVENESS, VALENCE, TEMPO, DURATION_MS, TIME_SIGNATURE
 ]
 """
-Spotify features, documentation: 
+Spotify features documentation: 
 https://developer.spotify.com/documentation/web-api/reference/#/operations/get-several-audio-features
 """
 
-# SPOTIFY TRACKS ------------------------------------------------------------
+# SPOTIFY RAW TRACKS ----------------------------------------------------------
 SONG_ID = 'song_id'
 SONG_NAME = 'song_name'
 SONG_NUMBER = 'song_number'
@@ -51,7 +57,7 @@ SONG_ARTISTS_NUMBER = 'song_artists_number'
 SPOTIFY_TRACKS_IDS_COLS = [ALBUM_ID, SONG_ID, SONG_NAME, SONG_NUMBER, SONG_ARTISTS_NUMBER]
 """Column names in output spotify track ids file."""
 
-# SPOTIFY PROCESSED DATA ------------------------------------------------------
+# SPOTIFY PROCESSED DATA -----------------------------------------------------
 NUM_TRACKS = 'num_tracks'
 NUM_FEATURES = 'num_features'
 
@@ -67,7 +73,11 @@ SPOTIFY_TRACKS_PROCESSED_COLS = [
 ]
 """Column names in output spotify processed tracks file."""
 
-# GENIUS STATS ----------------------------------------------------------------
+# SPOTIFY EXTRACTED FEATURES --------------------------------------------------
+
+SPOTIFY_CORE_FEATURES = [ALBUM_ID] + SPOTIFY_RAW_FEATURES
+
+# GENIUS DATA -----------------------------------------------------------------
 
 NUMBER_OF_LYRICS = 'number_of_fetched_lyrics'
 GENIUS_STATS_COLS = [ALBUM_ID, SPOTIFY_ALBUM, SPOTIFY_ARTIST, NUMBER_OF_LYRICS]
